41a4c79183fde6739e880f5a808f5d7459eb811c
じぃ <161315295+Ji-113@users.noreply.github.com>
2024-08-31 15:21:50 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener( 'DOMContentLoaded', function () {
   new Splide( '#card-carousel', {
 		perPage    : 2,
+	        cover      : true,
+		heightRatio: 0.5,
 		breakpoints: {
 			640: {
 				perPage: 1,
